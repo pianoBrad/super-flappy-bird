77e08ec987877911048f2c6b3d79471779631337
Brad Phillips <bradphillips@fabricatorz.com>
2014-02-12 01:38:17 -0600

    Add flappy bird font + fixed collision detection.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 var bird_height = $('.bird').outerHeight();
 var world_height = $('.world').outerHeight();
 var world_width = $('.world').outerWidth();
-var bounce_height = 100;
+var bounce_height = 80;
 var bounce_duration = 350;
 var pipe_width = 85;
-var pipe_top_height = 35;
-var pipe_bottom_height = 35;
+var pipe_top_height = 40;
+var pipe_bottom_height = 40;
 
 var pipe_assembly;
 var pipe_vs_bird;
+
+var current_pipe_position;
+var pipe_top_x;
+var pipe_bottom_x;
 
 var collision_test = function(element1, element2) {
       var x1 = element1.offset().left;
@@ -68,6 +72,8 @@ var start_pipe = function() {
 
 	if ( $('.pipe-wrap.next').length == 0 ) {
 		$('.pipe-wrap').addClass('next');
+		pipe_top_x = $('.pipe-wrap.next .pipe.top').outerHeight();
+		pipe_bottom_x = world_height - $('.pipe-wrap.next .pipe.bottom').outerHeight();
 	}
 
 	$('.pipe-wrap').animate({
@@ -88,29 +94,32 @@ var game_over = function() {
 }
 
 var is_pipe_still_next = function() {
-	var current_pipe_position = get_current_position( $('.pipe-wrap.next') );
+	current_pipe_position = get_current_position( $('.pipe-wrap.next') );
 	var current_bird_position = get_current_position( $('.bird') );
 
 	//console.log('test'+(current_pipe_position.left + pipe_width)+' '+current_bird_position.left);
 
-	if ( (current_pipe_position.left + pipe_width) < current_bird_position.left ) {
+	if ( (current_pipe_position.left + pipe_width) < current_bird_position.left && current_pipe_position.left > 0 ) {
+		console.log('clear!');
+		//alert('current pipe x is '+current_pipe_position.left+' + current bird position is '+current_bird_position.left+'..clear!');
 		var cleared_pipe = $('.pipe-wrap.next');
-		var next_pipe = cleared_pipe.next('.pipe-wrap')
+		var next_pipe = cleared_pipe.next('.pipe-wrap').addClass('next');
 		cleared_pipe.removeClass('next');
-		next_pipe.addClass('next');
+		current_pipe_position = get_current_position( $('.pipe-wrap.next') );
+		pipe_top_x = $('.pipe-wrap.next .pipe.top').outerHeight();
+		pipe_bottom_x = world_height - $('.pipe-wrap.next .pipe.bottom').outerHeight();
+		//alert('class removed from cleared pipe & added to next pipe..which has current position of '+(current_pipe_position.left + pipe_width));
 	}
 }
 
 var is_bird_touching = function() {
-	var pipe_top_x = $('.pipe-wrap .pipe.top').outerHeight();
-	var pipe_bottom_x = world_height - $('.pipe-wrap .pipe.bottom').outerHeight();
 
 	var current_bird_position = get_current_position( $('.bird') );
 	var current_bird_position_top = current_bird_position.top;
-	var current_bird_position_bottom = ( current_bird_position_top - bird_height );
+	var current_bird_position_bottom = ( current_bird_position_top + bird_height );
 	
-	if ( current_bird_position_top > pipe_top_x && current_bird_position_bottom < pipe_bottom_x ) {
-		console.log('fuck');
+	if ( pipe_top_x < current_bird_position_top  && pipe_bottom_x > current_bird_position_bottom ) {
+		console.log(pipe_top_x+' '+current_bird_position_top+' '+current_bird_position_bottom+' '+pipe_bottom_x);
 		return false;
 	} else {
 		console.log('pipe top is at '+pipe_top_x+', pipe bottom is at '+pipe_bottom_x+', bird is at '+current_bird_position_top+' '+current_bird_position_bottom+': yes');
@@ -126,6 +135,7 @@ $(document).ready(function() {
 	
 
 	$('body').one('click', function() {
+		$('.hud .get-ready').hide();
 		start_pipe();
 		pipe_assembly = window.setInterval(function() { start_pipe(); }, 1500)
 
